1bb80b7c32b89763105c0599e3ac64d45ddabd98
Bryan J Swift <bryan.j.swift@gmail.com>
2009-09-26 19:22:25 -0400

    Change temporaryWebappPath and webappPath to be same
    Making these paths the same means web-dev resources can be modified in
    the webapp folder without having to redeploy or mess with symlinks in
    order to have a productive environment. Since these paths were changed
    compiled classes and libraries need to be ignored by git.
    
    Also added jetty resources so jetty can be executed from within sbt and
    it will support the use of JSPs. Changed Jersey dependency to
    jersey-server because it contains the servlet needed.

--- project/build/WknycProject.scala
+++ project/build/WknycProject.scala
@@ -2,7 +2,7 @@ import sbt._
 import Process._
 
 class WknycProject(info:ProjectInfo) extends DefaultWebProject(info) {
-	// repository locations
+	// repository locations (for Jersey)
 	val javaNet = "Java.net Repository for Maven" at "http://download.java.net/maven/2/"
 	// dependencies for compiling
 	val commonsCollections = "commons-collections" % "commons-collections" % "3.2.1"
@@ -10,16 +10,22 @@ class WknycProject(info:ProjectInfo) extends DefaultWebProject(info) {
 	// Lucene
 	val luceneCore = "org.apache.lucene" % "lucene-core" % "2.9.0"
 	// Jersey
-	val jersey = "com.sun.jersey" % "jersey-core" % "1.1.2-ea"
+	val jersey = "com.sun.jersey" % "jersey-server" % "1.1.2-ea"
 
 	// Dependencies for testing
 	val junit = "junit" % "junit" % "4.5" % "test->default"
 	val specs = "org.scala-tools.testing" % "specs" % "1.6.0" % "test->default"
+	// Jetty
+	val jetty6 = "org.mortbay.jetty" % "jetty" % "6.1.14" % "test->default"
+	val jsp = "org.mortbay.jetty" % "jsp-2.1" % "6.1.14" % "test->default"
 
+	// tell sbt to find files normally under src/main/webapp under the webapp directory
+	override def webappPath = "webapp"
 	// override looking for jars in ./lib
 	override def dependencyPath = "src" / "main" / "lib"
-	// override output of war to target/webapp
-	override def temporaryWarPath = outputPath / "war"
+	// override output of exploded war to webapp directory
+	// webappPath and temporaryWarPath being the same means WEB-INF/lib doesn't get cleaned
+	override def temporaryWarPath = "webapp"
 	// override path to managed dependency cache
 	override def managedDependencyPath = "project" / "lib_managed"
 }
