c0631ec9ce287dc195662d8fd82dea73b8c4414f
Bryan J Swift <bryan.j.swift@gmail.com>
2009-09-28 01:58:41 -0400

    Change User class organization around a bit
    Now follows the pattern created by Content/ContentInfo trait and class

--- src/main/scala/wknyc/model/User.scala
+++ src/main/scala/wknyc/model/User.scala
@@ -1,19 +1,29 @@
 package wknyc.model
 
 import java.util.Date
+import javax.jcr.Credentials
 
 /** Contain access to all the fields necessary to be a user of the system */
-trait User {
-	def email:String
-	def password:String
-	def firstName:String
-	def lastName:String
-	def socialNetworks:List[SocialNetwork]
+trait User extends Credentials {
+	def credentials:WkCredentials
+	lazy val password = credentials.password
+	lazy val username = credentials.email
 }
+/** Represents interface to personal information for a user */
+trait Person {
+	def personalInfo:PersonalInfo
+	lazy val firstName = personalInfo.firstName
+	lazy val lastName = personalInfo.lastName
+	lazy val socialNetworks = personalInfo.socialNetworks
+}
+class PersonalInfo(val firstName:String, val lastName:String, val socialNetworks:List[SocialNetwork])
+class WkCredentials(val email:String, val password:String, val department:String, val title:String)
 /** Represent an Employee as a User */
 class Employee(
 	val contentInfo:ContentInfo,
-	val email:String, val password:String, val firstName:String,
-	val lastName:String, val socialNetworks:List[SocialNetwork],
-	val department:String, val title:String
-) extends User with Content
+	val credentials:WkCredentials,
+	val personalInfo:PersonalInfo
+) extends User with Person with Content {
+	lazy val department = credentials.department
+	lazy val title = credentials.title
+}
