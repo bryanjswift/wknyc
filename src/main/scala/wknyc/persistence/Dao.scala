720feeb65eee7544e6007d9c3b0a52683cfe1895
Bryan J Swift <bryan.j.swift@gmail.com>
2009-10-03 12:59:29 -0400

    Create UserDao and first spec
    Simple persister to save Employee objects (mostly). Has a hardcoded
    repository login using admin to the security workspace. Also sets the
    Employee properties manually before saving the session.

---
+++ src/main/scala/wknyc/persistence/Dao.scala
@@ -0,0 +1,29 @@
+package wknyc.persistence
+
+import javax.jcr.SimpleCredentials
+import wknyc.model.Employee
+
+object UserDao {
+	def save(employee:Employee) = {
+		val session = Config.Repository.login(new SimpleCredentials("admin","".toCharArray),"security")
+		try {
+			val root = session.getRootNode
+			val exists = root.hasNode(employee.username)
+			val eNode = if (exists) { root.getNode(employee.username) } else { root.addNode(employee.username) }
+			eNode.addMixin("mix:referenceable")
+			eNode.setProperty("username",employee.username)
+			eNode.setProperty("password",employee.password)
+			eNode.setProperty("department",employee.department)
+			eNode.setProperty("title",employee.title)
+			eNode.setProperty("firstName",employee.firstName)
+			eNode.setProperty("lastName",employee.lastName)
+			session.save
+			Some(eNode.getUUID)
+		} catch {
+			case e:Exception =>
+				None
+		} finally {
+			session.logout
+		}
+	}
+}
---
+++ src/test/scala/wknyc/persistence/DaoSpecs.scala
@@ -0,0 +1,18 @@
+package wknyc.persistence
+
+import org.specs.Specification
+import wknyc.model.{ContentInfo,Employee,PersonalInfo,SocialNetwork,WkCredentials}
+
+object UserDaoSpecs extends Specification {
+	val root = WkCredentials("root@wk.com","root","","")
+	val emp = Employee(
+			ContentInfo(root),
+			WkCredentials("bryan.swift@wk.com","bs","Digital","Software Engineer"),
+			PersonalInfo("Bryan","Swift",List[SocialNetwork]())
+		)
+	"UserDao" should {
+		"save an Employee" >> {
+			UserDao.save(emp) must beSome[String]
+		}
+	}
+}
