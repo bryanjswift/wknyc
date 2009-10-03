f248ddb2fed67e39b7155c7298218761be997620
Bryan J Swift <bryan.j.swift@gmail.com>
2009-10-03 12:55:36 -0400

    Make WkCredentials extend User trait
    Also making WkCredentials and PersonalInfo case classes so they can be
    created with companion objects and compared more easily. Add a companion
    object for Employee with an apply method for the default constructor.

--- src/main/scala/wknyc/model/User.scala
+++ src/main/scala/wknyc/model/User.scala
@@ -6,8 +6,8 @@ import javax.jcr.Credentials
 /** Contain access to all the fields necessary to be a user of the system */
 trait User extends Credentials {
 	def credentials:WkCredentials
-	lazy val password = credentials.password
-	lazy val username = credentials.email
+	val password:String = credentials.password
+	val username:String = credentials.email
 }
 /** Represents interface to personal information for a user */
 trait Person {
@@ -16,12 +16,17 @@ trait Person {
 	lazy val lastName = personalInfo.lastName
 	lazy val socialNetworks = personalInfo.socialNetworks
 }
-class PersonalInfo(val firstName:String, val lastName:String, val socialNetworks:List[SocialNetwork])
-class WkCredentials(val email:String, val password:String, val department:String, val title:String)
+case class PersonalInfo(val firstName:String, val lastName:String, val socialNetworks:List[SocialNetwork])
+case class WkCredentials(val email:String, override val password:String, val department:String, val title:String) extends User {
+	def credentials = this
+}
 /** Represent an Employee as a User */
 class Employee(
 	val contentInfo:ContentInfo, val credentials:WkCredentials, val personalInfo:PersonalInfo
 ) extends User with Person with Content {
 	lazy val department = credentials.department
 	lazy val title = credentials.title
+}
+object Employee {
+	def apply(ci:ContentInfo,credentials:WkCredentials,pi:PersonalInfo) = new Employee(ci,credentials,pi)
 }
