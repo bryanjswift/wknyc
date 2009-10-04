191e6990b8f3b39ec3a6421f407c6f4526c9f1a9
Bryan J Swift <bryan.j.swift@gmail.com>
2009-10-04 12:38:39 -0400

    Add uuid to User trait
    Use more concise case class syntax (leaving off val declarations).
    Update WkCredentials and Employee to implement uuid method. Add new
    apply method to Employee companion object to allow construction of
    Employee's with uuid.

--- src/main/scala/wknyc/model/User.scala
+++ src/main/scala/wknyc/model/User.scala
@@ -6,7 +6,8 @@ import javax.jcr.Credentials
 /** Contain access to all the fields necessary to be a user of the system */
 trait User extends Credentials {
 	def credentials:WkCredentials
-	val password:String = credentials.password
+	val uuid:Option[String]
+	val password:String = credentials.passw
 	val username:String = credentials.email
 }
 /** Represents interface to personal information for a user */
@@ -16,17 +17,20 @@ trait Person {
 	lazy val lastName = personalInfo.lastName
 	lazy val socialNetworks = personalInfo.socialNetworks
 }
-case class PersonalInfo(val firstName:String, val lastName:String, val socialNetworks:List[SocialNetwork])
-case class WkCredentials(val email:String, override val password:String, val department:String, val title:String) extends User {
+case class PersonalInfo(firstName:String, lastName:String, socialNetworks:List[SocialNetwork])
+case class WkCredentials(email:String, passw:String, department:String, title:String) extends User {
 	def credentials = this
+	val uuid = None
 }
 /** Represent an Employee as a User */
 class Employee(
-	val contentInfo:ContentInfo, val credentials:WkCredentials, val personalInfo:PersonalInfo
+	val contentInfo:ContentInfo, val credentials:WkCredentials, val personalInfo:PersonalInfo, val id:String
 ) extends User with Person with Content {
+	lazy val uuid = if (id.length > 0) Some(id) else None
 	lazy val department = credentials.department
 	lazy val title = credentials.title
 }
 object Employee {
-	def apply(ci:ContentInfo,credentials:WkCredentials,pi:PersonalInfo) = new Employee(ci,credentials,pi)
+	def apply(ci:ContentInfo,credentials:WkCredentials,pi:PersonalInfo) = new Employee(ci,credentials,pi,"")
+	def apply(ci:ContentInfo,credentials:WkCredentials,pi:PersonalInfo,id:String) = new Employee(ci,credentials,pi,id)
 }
