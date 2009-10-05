0b2ef076523f636a5362580f67f0851374eaf44b
Bryan J Swift <bryan.j.swift@gmail.com>
2009-10-05 14:44:57 -0400

    Add authenticate logic
    Well, at least start it. There are no user's created yet because the
    repository doesn't get boostrapped.

--- src/main/scala/wknyc/security/WkAuthentication.scala
+++ src/main/scala/wknyc/security/WkAuthentication.scala
@@ -4,21 +4,27 @@ import javax.jcr.{Credentials,RepositoryException,Session,SimpleCredentials}
 import org.apache.jackrabbit.core.security.authentication.Authentication
 import wknyc.model.WkCredentials
 
-class WkAuthentication(val systemSession:Session) extends Authentication {
+class WkAuthentication(private val systemSession:Session) extends Authentication {
 	def canHandle(credentials:Credentials):Boolean =
-		credentials.isInstanceOf[WkCredentials] || credentials.isInstanceOf[SimpleCredentials]
+		credentials.isInstanceOf[WkCredentials]
 
 	@throws(classOf[RepositoryException])
 	def authenticate(credentials:Credentials):Boolean = {
-		/*
 		if (canHandle(credentials)) {
-			systemSession.getNodeByUUID(null)
-			true
+			val creds = credentials.asInstanceOf[WkCredentials]
+			try {
+				val credsNode = systemSession.getNodeByUUID(creds.uuid.get)
+				val password = credsNode.getProperty("password")
+				password == creds.password
+				true
+			} catch {
+				case e:Exception =>
+					// user doesn't exist yet
+					true
+			}
 		} else {
 			false
 		}
-		*/
-		true
 	}
 
 }
