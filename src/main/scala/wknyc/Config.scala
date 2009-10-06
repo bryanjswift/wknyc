dde1f5b602a629a20d64f27b79e1c7c8ec830e0c
Bryan J Swift <bryan.j.swift@gmail.com>
2009-10-06 17:38:04 -0400

    Add registerNodeTypes method to Config

--- src/main/scala/wknyc/Config.scala
+++ src/main/scala/wknyc/Config.scala
@@ -1,7 +1,17 @@
 package wknyc
 
+import org.apache.jackrabbit.api.JackrabbitNodeTypeManager
 import org.apache.jackrabbit.core.TransientRepository
+import org.apache.jackrabbit.core.nodetype.NodeTypeManagerImpl
+import wknyc.model.WkCredentials
 
 object Config {
 	lazy val Repository = new TransientRepository()
+	def registerNodeTypes = {
+		val nodeTypeDefinition = getClass().getClassLoader().getResourceAsStream("wknyc/nodetype/types.cnd")
+		val session = Config.Repository.login(WkCredentials("admin@wk.com","","","",None))
+		val manager = session.getWorkspace().getNodeTypeManager().asInstanceOf[NodeTypeManagerImpl]
+		manager.registerNodeTypes(nodeTypeDefinition,JackrabbitNodeTypeManager.TEXT_X_JCR_CND)
+		session.logout
+	}
 }
