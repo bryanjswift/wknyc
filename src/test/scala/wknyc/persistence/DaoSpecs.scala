fadb6dc439664f9b403eadcc60ed916b5e86af91
Bryan J Swift <bryan.j.swift@gmail.com>
2009-10-06 12:23:19 -0400

    Change specs to work with updated Dao
    Dao is more aware of types and saved objects (uuids) so the order in
    which things are saved for testing had to be updated.

--- src/test/scala/wknyc/persistence/DaoSpecs.scala
+++ src/test/scala/wknyc/persistence/DaoSpecs.scala
@@ -1,21 +1,24 @@
 package wknyc.persistence
 
 import org.specs.Specification
-import wknyc.model.{ContentInfo,Employee,PersonalInfo,SocialNetwork,WkCredentials}
+import wknyc.model.{ContentInfo,Employee,PersonalInfo,SocialNetwork,User,WkCredentials}
 
 object UserDaoSpecs extends Specification {
-	val root = WkCredentials("root@wk.com","root","","",None)
-	val emp = Employee(
-			ContentInfo(root),
-			WkCredentials("bryan.swift@wk.com","bs","Digital","Software Engineer",None),
-			PersonalInfo("Bryan","Swift",List[SocialNetwork]())
-		)
+	var root:User = WkCredentials("root@wk.com","root","","",None)
 	"UserDao" should {
 		"save an Employee" >> {
 			val session = Config.Repository.login(WkCredentials("admin@wk.com","","","",None),"security")
 			try {
-				val dao = new UserDao(session,root)
-				dao.save(emp) must beSome[String]
+				var dao = new UserDao(session,root)
+				root = dao.save(root)
+				root.uuid must beSome[String]
+				dao = new UserDao(session,root)
+				val emp = Employee(
+						ContentInfo(root),
+						WkCredentials("bryan.swift@wk.com","bs","Digital","Software Engineer",None),
+						PersonalInfo("Bryan","Swift",List[SocialNetwork]())
+					)
+				dao.save(emp).uuid must beSome[String]
 			} finally {
 				session.logout
 			}
