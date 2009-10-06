72faee1f414e8b5db5b98a642e55e4a51a120d36
Bryan J Swift <bryan.j.swift@gmail.com>
2009-10-06 11:55:14 -0400

    Rewrite UserDao so it is DRYer
    save method now takes a type parameter determining which method to
    delegate to the new saveUser or saveEmployee methods. Also created
    writeProperities methods which create the data on a node for a User or
    Employee object. Refactored out the logic for retrieving a node so it
    isn't written in each saving method.
    
    Created getUser method to retrieve only the information needed for a
    User reference to avoid retrieving the whole User/Employee object graph
    when fetching an Employee with get. get method has also been updated to
    pull up ContentInfo data.

--- src/main/scala/wknyc/persistence/Dao.scala
+++ src/main/scala/wknyc/persistence/Dao.scala
@@ -1,6 +1,6 @@
 package wknyc.persistence
 
-import javax.jcr.Session
+import javax.jcr.{Node,Session}
 import wknyc.model.{ContentInfo,Employee,PersonalInfo,SocialNetwork,User,WkCredentials}
 
 /** UserDao is created to save and retrieve User type objects from the repository
@@ -8,53 +8,134 @@ import wknyc.model.{ContentInfo,Employee,PersonalInfo,SocialNetwork,User,WkCrede
 	* @param loggedInUser is used to set lastModifiedUser of content being saved
 	*/
 class UserDao(private val session:Session, private val loggedInUser:User) {
-	// should return an employee object with uuid's added (if new)
-	// date created and date modified values should also be updated
-	def save(employee:Employee) = {
-		try {
-			val root = session.getRootNode
-			val exists = root.hasNode(employee.username)
-			val n = if (exists) { root.getNode(employee.username) } else { root.addNode(employee.username) }
-			if (exists) {
-				n.checkout
-			} else {
-				n.addMixin("mix:referenceable")
-				n.addMixin("mix:versionable")
-			}
-			n.setProperty("username",employee.username)
-			n.setProperty("password",employee.password)
-			n.setProperty("department",employee.department)
-			n.setProperty("title",employee.title)
-			n.setProperty("firstName",employee.firstName)
-			n.setProperty("lastName",employee.lastName)
-			session.save
-			n.checkin
-			Some(n.getUUID)
-		} catch {
-			case e:Exception =>
-				None
+	/** Save an object which is at least of type User
+		* @param user to be saved delegates to saveUser or saveEmployee depending on type
+		* @returns T with uuid populated and lastModified/modifiedBy fields updated (if applicable)
+		*/
+	def save[T >: User](user:T):T =
+		user match {
+			case employee:Employee => saveEmployee(employee)
+			case u:User => saveUser(u)
+		}
+	/** Save a general User object to the repository
+		* @param user to be saved
+		* @returns User with uuid populated
+		*/
+	private def saveUser(user:User) = {
+		val n = getNode(user.username)
+		writeProperties(n,user)
+		session.save
+		n.checkin
+		new User {
+			val credentials:WkCredentials = null // I don't like this
+			override val username = user.username
+			override val password = user.password
+			override val department = user.department
+			override val title = user.title
+			val uuid = Some(n.getUUID)
 		}
 	}
-	private def saveUser(user:User) = {
+	/** Save an Employee to the repository
+		* @param employee to be saved
+		* @returns Employee with uuid, lastModified and modifiedBy updated
+		*/
+	private def saveEmployee(employee:Employee) = {
+		val ci = employee.contentInfo.modify(loggedInUser) // do it first so saved data and object have same values
+		val n = getNode(employee.username)
+		writeProperties(n,employee,ci)
+		session.save
+		n.checkin
+		Employee(
+			ci,
+			employee.credentials,
+			employee.personalInfo,
+			n.getUUID
+		)
 	}
+	/** Create/retrieve a referenceable and versionable node from the root of the session's workspace
+		* @param name of the node to retrieve
+		* @returns Node with given name (as path)
+		*/
+	private def getNode(name:String):Node = getNode(session.getRootNode,name)
+	/** Create/retrieve a referenceable and versionable node from the given parent node
+		* @param parent node to search from
+		* @param name of the node to retrieve
+		* @returns Node with given name (as path)
+		*/
+	private def getNode(parent:Node,name:String):Node =
+		if (parent.hasNode(name)) {
+			val node = parent.getNode(name)
+			node.checkout
+			node
+		} else {
+			val node = parent.addNode(name)
+			node.addMixin("mix:referenceable")
+			node.addMixin("mix:versionable")
+			node
+		}
+	/** Write general user properties to provided node
+		* @param n - node to write data to
+		* @param user whose data should be written
+		*/
+	private def writeProperties(n:Node,user:User):Unit = {
+		n.setProperty("username",user.username)
+		n.setProperty("password",user.password)
+		n.setProperty("department",user.department)
+		n.setProperty("title",user.title)
+	}
+	/** Write employee information to provided node
+		* @param n - node to write data to
+		* @param employee object containing data to write
+		* @param ci ContentInfo with datestamps and user tracking to write
+		*/
+	private def writeProperties(n:Node,employee:Employee,ci:ContentInfo):Unit = {
+		writeProperties(n,employee)
+		n.setProperty("firstName",employee.firstName)
+		n.setProperty("lastName",employee.lastName)
+		n.setProperty("dateCreated",ci.dateCreated)
+		n.setProperty("lastModified",ci.lastModified)
+		n.setProperty("modifiedBy", session.getNodeByUUID(loggedInUser.uuid.get))
+	}
+	/** Fetch an Employee based on a given UUID
+		* @param uuid of data to be fetched
+		* @returns Employee built from data in uuid's node
+		*/
 	def get(uuid:String) = {
-		try {
-			val n = session.getNodeByUUID(uuid)
-			Employee(
-				ContentInfo(WkCredentials("","","","",None)),
-				WkCredentials(
-					n.getProperty("username").getString,
-					n.getProperty("password").getString,
-					n.getProperty("department").getString,
-					n.getProperty("title").getString,
-					None
-				),
-				PersonalInfo(
-					n.getProperty("firstName").getString,
-					n.getProperty("lastName").getString,
-					List[SocialNetwork]()
-				)
-			)
+		val n = session.getNodeByUUID(uuid)
+		Employee(
+			new ContentInfo(
+				n.getProperty("dateCreated").getDate,
+				n.getProperty("lastModified").getDate,
+				getUser(n.getProperty("modifiedBy").getNode.getUUID)
+			),
+			WkCredentials(
+				n.getProperty("username").getString,
+				n.getProperty("password").getString,
+				n.getProperty("department").getString,
+				n.getProperty("title").getString,
+				Some(uuid)
+			),
+			PersonalInfo(
+				n.getProperty("firstName").getString,
+				n.getProperty("lastName").getString,
+				List[SocialNetwork]()
+			),
+			uuid
+		)
+	}
+	/** Fetch a simple user from a given UUID so we don't fetch the whole user graph when retrieving Employee's modifiedBy
+		* @param uuid of data to fetch
+		* @returns User built from uuid's node
+		*/
+	private def getUser(uuid:String) = {
+		val n = session.getNodeByUUID(uuid)
+		new User {
+			val credentials:WkCredentials = null // I don't like this
+			override val username = n.getProperty("username").getString
+			override val password = n.getProperty("password").getString
+			override val department = n.getProperty("department").getString
+			override val title = n.getProperty("title").getString
+			val uuid = Some(n.getUUID)
 		}
 	}
 }
