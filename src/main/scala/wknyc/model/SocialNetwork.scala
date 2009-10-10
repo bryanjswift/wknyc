f64fecb6cf20ceeef55b370da35a1fb2b139bdd3
Bryan J Swift <bryan.j.swift@gmail.com>
2009-10-09 21:15:10 -0400

    Make SocialNetwork a respectable class

--- src/main/scala/wknyc/model/SocialNetwork.scala
+++ src/main/scala/wknyc/model/SocialNetwork.scala
@@ -1,4 +1,22 @@
 package wknyc.model
 
 /** Represents a social networking site */
-class SocialNetwork(val name:String, val url:String)
+class SocialNetwork(val name:String, val url:String) {
+	private def canEqual(a:Any) = a.isInstanceOf[SocialNetwork]
+	def equals(sn:SocialNetwork) =
+		this.name == sn.name && this.url == sn.url
+	override def equals(q:Any) =
+		q match {
+			case that:SocialNetwork =>
+				canEqual(q) && equals(that)
+			case _ => false
+		}
+	override def hashCode =
+		41 * (41 + name.hashCode) + url.hashCode
+	override def toString = (new StringBuilder("SocialNetwork(")).append(name).append(",").append(url).append(")").toString
+}
+
+object SocialNetwork {
+	val NodeType = "wk:socialNetwork"
+	def apply(name:String,url:String) = new SocialNetwork(name,url)
+}
