aa12c3ff76ca9975470defb66c37dbaab8d30247
Bryan J Swift <bryan.j.swift@gmail.com>
2009-10-09 20:47:26 -0400

    Stick to just the generic registerNodeTypes method
    Remove registerSecurityNodeTypes and registerDefaultNodeTypes methods
    which were specific to workspaces and generalize process to fit any
    workspace. Make registered Map private in WkRepository.

--- src/main/scala/wknyc/Config.scala
+++ src/main/scala/wknyc/Config.scala
@@ -1,6 +1,7 @@
 package wknyc
 
 import javax.jcr.{Credentials,Session}
+import javax.jcr.nodetype.NodeType
 import org.apache.jackrabbit.api.JackrabbitNodeTypeManager
 import org.apache.jackrabbit.core.TransientRepository
 import org.apache.jackrabbit.core.nodetype.NodeTypeManagerImpl
@@ -12,27 +13,27 @@ object Config {
 	lazy val ContentWorkspace = "default"
 	lazy val Admin = WkCredentials("admin@wk.com","","","",None)
 	lazy val ClassLoader = getClass.getClassLoader
-	private def registerNodeTypes(filename:String,session:Session):Unit = {
+	def registerNodeTypes(session:Session):Array[NodeType] =
+		session.getWorkspace.getName match {
+			case CredentialsWorkspace => registerNodeTypes("security.cnd",session)
+			case ContentWorkspace => registerNodeTypes("default.cnd",session)
+			case _ => Array[NodeType]()
+		}
+	private def registerNodeTypes(filename:String,session:Session):Array[NodeType] = {
 		val definitions = ClassLoader.getResourceAsStream("wknyc/nodetype/" + filename)
 		val manager = session.getWorkspace().getNodeTypeManager().asInstanceOf[NodeTypeManagerImpl]
 		manager.registerNodeTypes(definitions,JackrabbitNodeTypeManager.TEXT_X_JCR_CND,true)
 	}
-	def registerSecurityNodeTypes(session:Session):Unit = registerNodeTypes("security.cnd",session)
-	def registerDefaultNodeTypes(session:Session):Unit = registerNodeTypes("default.cnd",session)
 }
 
 class WkRepository extends TransientRepository {
-	var registered = Map[String,Boolean]()
+	private var registered = Map[String,Boolean]()
 	override def login(credentials:Credentials,workspaceName:String) = {
 		val session = super.login(credentials,workspaceName)
 		val workspace = session.getWorkspace.getName
 		if (!registered.getOrElse(workspace,false)) {
 			registered += (workspace -> true)
-			workspace match {
-				case Config.CredentialsWorkspace => Config.registerSecurityNodeTypes(session)
-				case Config.ContentWorkspace => Config.registerDefaultNodeTypes(session)
-				case _ => ()
-			}
+			Config.registerNodeTypes(session)
 		}
 		session
 	}
