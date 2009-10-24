9b8f5cfe5969c156cc6e359abbc44567277272de
Bryan J Swift <bryan.j.swift@gmail.com>
2009-10-24 15:38:57 -0400

    Add spec for saving a CaseStudy

---
+++ src/test/scala/wknyc/persistence/ClientDaoSpecs.scala
@@ -0,0 +1,43 @@
+package wknyc.persistence
+
+import org.specs.Specification
+import wknyc.model._
+
+object ClientDaoSpecs extends Specification {
+	"ClientDao" should {
+		shareVariables()
+		setSequential()
+		val securitySession = Config.Repository.login(Config.Admin,Config.CredentialsWorkspace)
+		val session = Config.Repository.login(Config.Admin,Config.ContentWorkspace)
+		val userDao = new UserDao(securitySession,Config.Admin)
+		val root = userDao.save(WkCredentials("root@wk.com","root","","",None))
+		val assetDao = new AssetDao(session,root)
+		val dao = new ClientDao(session,root)
+		doAfterSpec {
+			dao.close
+			assetDao.close
+			securitySession.logout
+			session.logout
+		}
+		"save a CaseStudy" >> {
+			val copy = assetDao.save(CopyAsset(
+					ContentInfo(root),
+					"Title",
+					<p>Just a test</p>
+				))
+			val caseStudy =
+				CaseStudy(
+					ContentInfo(root),
+					copy,
+					List[DownloadableAsset](),
+					"Headline",
+					"name",
+					List[PressAsset](),
+					List[CaseStudy](),
+					"study type",
+					List[String]()
+				)
+			dao.save(caseStudy).uuid must beSome[String]
+		}
+	}
+}
