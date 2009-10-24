bc777ae7fbf2a0d4c4db76816d4cb6b686d3acee
Bryan J Swift <bryan.j.swift@gmail.com>
2009-10-23 22:00:47 -0400

    Add cp method to CaseStudy and Client

--- src/main/scala/wknyc/model/Client.scala
+++ src/main/scala/wknyc/model/Client.scala
@@ -12,7 +12,9 @@ class CaseStudy(
 	val related:List[CaseStudy],
 	val studyType:String,
 	val tags:List[String]
-) extends Content
+) extends Content {
+	def cp(uuid:String) = new CaseStudy(contentInfo.cp(uuid),assets,description,downloads,headline,name,press,related,studyType,tags)
+}
 
 object CaseStudy {
 	val NodeType = "wk:caseStudy"
@@ -27,7 +29,9 @@ object CaseStudy {
 	val Tags = "tags"
 }
 /** Represent a collection of CaseStudy objects */
-class Client(val contentInfo:ContentInfo, val name:String, val caseStudies:List[CaseStudy]) extends Content
+class Client(val contentInfo:ContentInfo, val name:String, val caseStudies:List[CaseStudy]) extends Content {
+	def cp(uuid:String) = new Client(contentInfo.cp(uuid), name, caseStudies)
+}
 object Client {
 	val NodeType = "wk:client"
 	val Name = "name"
