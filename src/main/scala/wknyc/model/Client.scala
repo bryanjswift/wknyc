93645e47849595cc65ff96da15e6bfdcb5de0013
Bryan J Swift <bryan.j.swift@gmail.com>
2009-10-23 21:25:44 -0400

    Reorganize properties and add info for persistence

--- src/main/scala/wknyc/model/Client.scala
+++ src/main/scala/wknyc/model/Client.scala
@@ -2,17 +2,34 @@ package wknyc.model
 
 /** Represent the Case Study concept which is the primary display unit */
 class CaseStudy(
+	val contentInfo:ContentInfo,
 	val assets:List[Asset],
-	val headline:String,
-	val copy:CopyAsset,
-	val press:List[PressAsset],
+	val description:CopyAsset,
 	val downloads:List[DownloadableAsset],
-	val studyType:String,
-	val related:List[CaseStudy],
-	val tags:List[String]
-)
-/** Represent a collection of CaseStudy objects */
-class Client(
+	val headline:String,
 	val name:String,
-	val caseStudies:List[CaseStudy]
-)
+	val press:List[PressAsset],
+	val related:List[CaseStudy],
+	val studyType:String,
+	val tags:List[String]
+) extends Content
+
+object CaseStudy {
+	val NodeType = "wk:caseStudy"
+	val Assets = "assets"
+	val Description = "description"
+	val Downloads = "downloads"
+	val Headline = "headline"
+	val Name = "name"
+	val Press = "press"
+	val Related = "related"
+	val StudyType = "studyType"
+	val Tags = "tags"
+}
+/** Represent a collection of CaseStudy objects */
+class Client(val contentInfo:ContentInfo, val name:String, val caseStudies:List[CaseStudy]) extends Content
+object Client {
+	val NodeType = "wk:client"
+	val Name = "name"
+	val CaseStudies = "caseStudies"
+}
