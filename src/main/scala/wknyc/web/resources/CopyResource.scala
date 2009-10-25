50e0368f5e5a766835710850279688cb90aa40b6
Bryan J Swift <bryan.j.swift@gmail.com>
2009-10-25 16:38:14 -0400

    Rearrange imports

--- src/main/scala/wknyc/web/resources/CopyResource.scala
+++ src/main/scala/wknyc/web/resources/CopyResource.scala
@@ -2,15 +2,15 @@ package wknyc.web.resources
 
 import com.sun.jersey.spi.resource.Singleton
 import javax.ws.rs.{GET,Produces,Path,PathParam}
-import javax.ws.rs.core.MediaType.{APPLICATION_XML,TEXT_PLAIN,TEXT_XML}
+import javax.ws.rs.core.MediaType.APPLICATION_XML
 
 @Singleton
 @Path("/copy")
 class CopyResource {
 	@GET
 	@Path("/single/{title}")
-	@Produces(Array(APPLICATION_XML,TEXT_PLAIN,TEXT_XML))
-	def getCopy(@PathParam("title") title:String):String = 
+	@Produces(Array(APPLICATION_XML))
+	def getCopy(@PathParam("title") title:String):String =
 		<Message>Hello Stupid.. looking for {title}</Message> toString
 }
 
