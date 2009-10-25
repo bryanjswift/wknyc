0bf3d64645fc4085480418fb4abaa6b930fd21ae
Bryan J Swift <bryan.j.swift@gmail.com>
2009-10-25 16:39:41 -0400

    Use Response objects
    With the create NodeSeq @Provider the RegisterResource can build
    Response objects with XML as the entity

--- src/main/scala/wknyc/web/resources/RegisterResource.scala
+++ src/main/scala/wknyc/web/resources/RegisterResource.scala
@@ -2,7 +2,7 @@ package wknyc.web.resources
 
 import com.sun.jersey.spi.resource.Singleton
 import javax.ws.rs.{POST,Produces,Path,FormParam}
-import javax.ws.rs.core.MediaType.APPLICATION_XML
+import javax.ws.rs.core.{MediaType,Response}
 import wknyc.Config
 import wknyc.model.{ContentInfo,Employee,PersonalInfo,SocialNetwork,WkCredentials}
 import wknyc.persistence.UserDao
@@ -12,27 +12,29 @@ import wknyc.persistence.UserDao
 class RegisterResource {
 	@POST
 	@Path("/register")
-	@Produces(Array(APPLICATION_XML))
+	@Produces(Array(MediaType.APPLICATION_XML))
 	def registerUser(
 		@FormParam("username") username:String,
 		@FormParam("password") password:String,
 		@FormParam("department") department:String,
 		@FormParam("title") title:String
-	):String = {
+	) = {
 		val session = Config.Repository.login(Config.Admin,Config.CredentialsWorkspace)
 		val dao = new UserDao(session,Config.Admin)
 		try {
 			val user = dao.save(WkCredentials(username,password,department,title,None))
-			<Response>
-				<Status>200</Status>
-				<Message>Credentials successfully created for {user.username}</Message>
-				<Credentials>
-					<UUID>{user.uuid.get}</UUID>
-					<Username>{user.username}</Username>
-					<Department>{user.department}</Department>
-					<Title>{user.title}</Title>
-				</Credentials>
-			</Response> toString
+			val xml =
+				<Response>
+					<Status>200</Status>
+					<Message>Credentials successfully created for {user.username}</Message>
+					<Credentials>
+						<UUID>{user.uuid.get}</UUID>
+						<Username>{user.username}</Username>
+						<Department>{user.department}</Department>
+						<Title>{user.title}</Title>
+					</Credentials>
+				</Response>
+			Response.status(Response.Status.OK).entity(xml).build
 		} finally {
 			dao.close
 			session.logout
@@ -40,7 +42,7 @@ class RegisterResource {
 	}
   @POST
   @Path("/registerEmployee")
-  @Produces(Array(APPLICATION_XML))
+	@Produces(Array(MediaType.APPLICATION_XML))
   def registerEmployee(
 		@FormParam("username") username:String,
 		@FormParam("password") password:String,
@@ -48,7 +50,7 @@ class RegisterResource {
 		@FormParam("title") title:String,
 		@FormParam("firstName") firstName:String,
 		@FormParam("lastName") lastName:String
-  ):String = {
+  ) = {
 		val session = Config.Repository.login(Config.Admin,Config.CredentialsWorkspace)
 		val dao = new UserDao(session,Config.Admin)
 		try {
@@ -57,18 +59,20 @@ class RegisterResource {
 				WkCredentials(username,password,department,title,None),
 				PersonalInfo(firstName,lastName,List[SocialNetwork]())
 			))
-			<Response>
-				<Status>200</Status>
-				<Message>Employee successfully created for {employee.username}</Message>
-				<Credentials>
-					<UUID>{employee.uuid.get}</UUID>
-					<Username>{employee.username}</Username>
-					<Department>{employee.department}</Department>
-					<Title>{employee.title}</Title>
-					<FirstName>{employee.firstName}</FirstName>
-					<LastName>{employee.lastName}</LastName>
-				</Credentials>
-			</Response> toString
+			val xml =
+				<Response>
+					<Status>200</Status>
+					<Message>Employee successfully created for {employee.username}</Message>
+					<Credentials>
+						<UUID>{employee.uuid.get}</UUID>
+						<Username>{employee.username}</Username>
+						<Department>{employee.department}</Department>
+						<Title>{employee.title}</Title>
+						<FirstName>{employee.firstName}</FirstName>
+						<LastName>{employee.lastName}</LastName>
+					</Credentials>
+				</Response>
+			Response.status(Response.Status.OK).entity(xml).build
 		} finally {
 			dao.close
 			session.logout
