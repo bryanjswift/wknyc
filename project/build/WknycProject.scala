91e19010fdd2fd7e4f978e8e1fa4fc0abe91cca2
Bryan J Swift <bryan.j.swift@gmail.com>
2009-10-26 01:30:19 -0400

    Pull some tasks out into plugins
    Update Project definition to use new plugins

--- project/build/WknycProject.scala
+++ project/build/WknycProject.scala
@@ -1,6 +1,8 @@
+import bjs.jackrabbit.CleanRepositoryPlugin
+import bjs.webapp.CleanWebappPlugin
 import sbt._
 
-class WknycProject(info:ProjectInfo) extends DefaultWebProject(info) {
+class WknycProject(info:ProjectInfo) extends DefaultWebProject(info) with CleanWebappPlugin with CleanRepositoryPlugin {
 	// repository locations (for Jersey)
 	val javaNet = "Java.net Repository for Maven" at "http://download.java.net/maven/2/"
 
@@ -22,33 +24,6 @@ class WknycProject(info:ProjectInfo) extends DefaultWebProject(info) {
 	val jetty6 = "org.mortbay.jetty" % "jetty" % "6.1.14" % "test->default"
 	val jsp = "org.mortbay.jetty" % "jsp-2.1" % "6.1.14" % "test->default"
 
-	// Define task to delete webapp class and jar files
-	lazy val cleanWebapp = task {
-		val toClean = webappPath / "WEB-INF" / "classes" ** "*" +++ webappPath / "WEB-INF" / "lib" * "*.jar"
-		try {
-			toClean.getFiles.foreach(_.delete)
-			None
-		} catch {
-			case e:Exception => 
-				Some(e.getMessage)
-		}
-	}
-
-	// Define task to clean up repository from filesystem
-	lazy val cleanRepository = task {
-		def deleteDirectory(path:java.io.File):Boolean = {
-			path.listFiles.foreach(f => if (f.isDirectory) deleteDirectory(f) else f.delete)
-			path.delete
-		}
-		val repositoryPath = "." / "repository"
-		try {
-			repositoryPath.getFiles.foreach(deleteDirectory)
-			None
-		} catch {
-			case e:Exception =>
-				Some(e.getMessage)
-		}
-	}
 	// override clean action to depend on cleanWebapp
 	override def cleanAction = super.cleanAction dependsOn(cleanRepository) dependsOn(cleanWebapp)
 	// tell sbt to find files normally under src/main/webapp under the webapp directory
