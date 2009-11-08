e308d24c987068fdaa9515018bfb8758c9de3267
Bryan J Swift <bryan.j.swift@gmail.com>
2009-11-08 14:15:39 -0500

    Change registered to a Set[String]
    Didn't need to be a Map because all it was being used for was to check
    if something existed in it

--- src/main/scala/wknyc/WkRepository.scala
+++ src/main/scala/wknyc/WkRepository.scala
@@ -4,14 +4,14 @@ import javax.jcr.{Credentials,Session}
 import org.apache.jackrabbit.core.{SessionImpl,TransientRepository}
 
 class WkRepository extends TransientRepository {
-	private var registered = Map[String,Boolean]()
+	private var registered = Set[String]()
 	private var sessions = Set[Session]()
 	override def login(credentials:Credentials,workspaceName:String) = {
 		val session = super.login(credentials,workspaceName)
 		sessions += session
 		val workspace = session.getWorkspace.getName
-		if (!registered.getOrElse(workspace,false)) {
-			registered += (workspace -> true)
+		if (!registered.contains(workspace)) {
+			registered += workspace
 			Config.registerNodeTypes(session)
 		}
 		//Console.println("WkRepository::login - Num. Sessions: " + sessions.size)
@@ -20,7 +20,7 @@ class WkRepository extends TransientRepository {
 	override def loggedOut(session:SessionImpl) = {
 		sessions -= session
 		if (sessions.isEmpty) {
-			registered = Map[String,Boolean]()
+			registered = Set[String]()
 		}
 		//Console.println("WkRepository::loggedOut - Num. Sessions: " + sessions.size)
 		super.loggedOut(session)
