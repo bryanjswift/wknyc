ab09ede5a35d6f4a235836ddca606b102f5008dd
Bryan J Swift <bryan.j.swift@gmail.com>
2009-11-08 15:15:19 -0500

    Add startup servlet to keep a Repository Session open
    This is so the repository doesn't shutdown and so doesn't cost startup
    time whenever due to inactivity. There may be long term performance
    costs.

---
+++ src/main/scala/wknyc/web/servlets/RepositoryStartupServlet.scala
@@ -0,0 +1,14 @@
+package wknyc.web.servlets
+
+import javax.servlet.{GenericServlet,ServletConfig,ServletContext,ServletRequest => Request,ServletResponse => Response}
+
+class RepositoryStartupServlet extends GenericServlet {
+	private var session = Config.Repository.login(Config.Admin,Config.CredentialsWorkspace)
+	override def init(config:ServletConfig) = {
+		super.init(config)
+	}
+	override def service(request:Request,response:Response) = { }
+	override def destroy() = {
+		session.logout
+	}
+}
