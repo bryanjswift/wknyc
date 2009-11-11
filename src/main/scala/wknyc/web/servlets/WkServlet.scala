5e8a728b854e21539c3ab82bcae3748c81c79638
Bryan J Swift <bryan.j.swift@gmail.com>
2009-11-11 00:12:32 -0500

    Change getParameter to return String not Option[String]
    Remove unused import. Change method signature to have 3 parameter lists.

--- src/main/scala/wknyc/web/servlets/WkServlet.scala
+++ src/main/scala/wknyc/web/servlets/WkServlet.scala
@@ -1,13 +1,13 @@
 package wknyc.web.servlets
 
-import javax.servlet.http.{HttpServlet, HttpServletRequest => Request, HttpServletResponse => Response}
+import javax.servlet.http.{HttpServletRequest => Request, HttpServletResponse => Response}
 import org.apache.commons.fileupload.FileItemStream
 
 trait WkServlet {
 	implicit val default = ""
-	protected def getParameter(request:Request,param:String)(implicit default:String) = {
+	protected def getParameter(request:Request)(param:String)(implicit default:String) = {
 		val value = request.getParameter(param)
-		if (value == default || value == null) { None }
-		else { Some(value) }
+		if (value == default || value == null) { this.default }
+		else { value }
 	}
 }
