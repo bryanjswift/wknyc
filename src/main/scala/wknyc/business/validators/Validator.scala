6b69ac0cc42628e0f7505cdc4668bde8ddb79b2d
Bryan J Swift <bryan.j.swift@gmail.com>
2009-11-11 23:20:22 -0500

    Change from emptyRE to ""

--- src/main/scala/wknyc/business/validators/Validator.scala
+++ src/main/scala/wknyc/business/validators/Validator.scala
@@ -2,12 +2,11 @@ package wknyc.business.validators
 
 trait Validator {
 	// General Regular Expressions
-	protected val emptyRE = "^$".r
 	// Generic Helpers
 	protected def notValidated(field:String) = ValidationSuccess(field)
 	protected def required(value:String,field:String) =
 		value match {
-			case emptyRE(empty) => ValidationError(field,String.format("%s can not be empty.",field))
+			case "" => ValidationError(field,String.format("%s can not be empty.",field))
 			case _ => ValidationSuccess(field)
 		}
 	def validate(v:AnyRef):List[ValidationResult]
