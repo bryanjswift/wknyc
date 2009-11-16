5f7f96a12e6ff621db38b334e3f505a6f23d55e7
Bryan Swift <bryan.j.swift@gmail.com>
2009-11-16 03:08:20 -0500

    Add helper methods to make getting at results easier
    Add case class for holding different types of results.

--- src/main/scala/wknyc/business/validators/Validator.scala
+++ src/main/scala/wknyc/business/validators/Validator.scala
@@ -1,6 +1,11 @@
 package wknyc.business.validators
 
 trait Validator {
+	case class ValidationResults(all:List[ValidationResult]) {
+		private val (success,error) = all.partition(_.isInstanceOf[ValidationSuccess])
+		val errors = error.asInstanceOf[List[ValidationError]]
+		val successes = success.asInstanceOf[List[ValidationSuccess]]
+	}
 	// General Regular Expressions
 	// Generic Helpers
 	protected def notValidated(field:String) = ValidationSuccess(field)
@@ -9,5 +14,9 @@ trait Validator {
 			case "" => ValidationError(field,String.format("%s can not be empty.",field))
 			case _ => ValidationSuccess(field)
 		}
-	def validate(v:AnyRef):List[ValidationResult]
+	// Worker methods
+	def validate(a:AnyRef):List[ValidationResult]
+	def errors(a:AnyRef) = results(a).errors
+	def results(a:AnyRef) = ValidationResults(validate(a))
+	def successes(a:AnyRef) = results(a).successes
 }
