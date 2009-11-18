208fb6a79c08a7048ea8792b7f9b8b42ac77e325
Bryan Swift <bryan.j.swift@gmail.com>
2009-11-18 04:11:04 -0500

    Add list and getByName method to ClientManager

--- src/main/scala/wknyc/business/ClientManager.scala
+++ src/main/scala/wknyc/business/ClientManager.scala
@@ -6,4 +6,6 @@ import wknyc.persistence.ClientDao
 
 object ClientManager extends Manager {
 	def get(uuid:String) = using(new ClientDao(Config.Admin)) { _.get(uuid) }
+	def list = using(new ClientDao(Config.Admin)) { _.list }
+	def getByName(name:String) = list.find(_.name == name)
 }
