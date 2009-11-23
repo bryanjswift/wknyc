40abc1a6f94afe1de0f238f6c482aa0c79084618
Bryan J Swift <bryan.j.swift@gmail.com>
2009-11-23 10:55:04 -0500

    Update save method to create new ContentInfo if the info of Client is Empty

--- src/main/scala/wknyc/business/ClientManager.scala
+++ src/main/scala/wknyc/business/ClientManager.scala
@@ -1,7 +1,7 @@
 package wknyc.business
 
 import wknyc.WkPredef._
-import wknyc.model.{Client,User}
+import wknyc.model.{Client,ContentInfo,User}
 import wknyc.persistence.ClientDao
 import validators.{ClientValidator,Error,ValidationError,ValidationResult}
 
@@ -9,13 +9,25 @@ object ClientManager extends Manager {
 	def get(uuid:String) = using(new ClientDao(Config.Admin)) { _.get(uuid) }
 	def getByName(name:String) = list.find(_.name == name)
 	def list = using(new ClientDao(Config.Admin)) { _.list }
+	/** Save the Client instance
+	 * If instance has Empty ContentInfo then populate it before saving
+	 * @param client - instance to save
+	 * @param loggedIn - user which is currently logged in to the application
+	 * @returns Response[Client] with the errors or the updated Client
+	 */
 	def save(client:Client,loggedIn:User):Response[Client] = {
-		val errors = ClientValidator.errors(client)
+		val c =
+			if (client.contentInfo == ContentInfo.Empty) {
+				client.cp(ContentInfo(loggedIn))
+			} else {
+				client
+			}
+		val errors = ClientValidator.errors(c)
 		errors match {
 			case Nil =>
 				using(new ClientDao(loggedIn))(dao =>
 					try {
-						Success(dao.save(client))
+						Success(dao.save(c))
 					} catch {
 						case e:Exception => Failure(List(Error(e)),"Unable to create/update CaseStudy " + client.name)
 					}
