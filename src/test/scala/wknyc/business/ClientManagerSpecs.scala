6b3464b5c17d0cf4e2db19a669c864db0d03257e
Bryan Swift <bryan.j.swift@gmail.com>
2009-11-25 01:54:47 -0500

    Clean up specs for ClientManager
    Add spec for successfully saving client

--- src/test/scala/wknyc/business/ClientManagerSpecs.scala
+++ src/test/scala/wknyc/business/ClientManagerSpecs.scala
@@ -9,18 +9,19 @@ object ClientManagerSpecs extends Specification {
 	val validClient = Client(ContentInfo(Config.Admin),"Test Client",List[CaseStudy]())
 	"ClientManager.save" should {
 		"produce a Failure when not given a user" >> {
-			ClientManager.save(validClient,None) match {
-				case Failure(errors,message) =>
-					errors.size must beGreaterThan(0)
-				case Success(payload,message) => fail("Failure result should be produced for non-existant user")
-			}
+			val response = ClientManager.save(validClient,None)
+			response.errors.size must beGreaterThan(0)
+			response.errors mustExist(_.field == "user")
 		}
 		"produce a Failure when given an invalid Client" >> {
-			ClientManager.save(invalidClient,Some(Config.Admin)) match {
-				case Failure(errors,message) =>
-					errors.size must beGreaterThan(0)
-				case Success(payload,message) => fail("Failure result should be produced for invalid client")
-			}
+			val response = ClientManager.save(invalidClient,Some(Config.Admin))
+			response.errors.size must beGreaterThan(0)
+			response.errors mustExist(_.field == Client.Name)
+		}
+		"succeed when given a valid Client and User" >> {
+			val response = ClientManager.save(validClient,Some(Config.Admin))
+			response.errors.size mustBe 0
+			response.payload.uuid must beSome[String]
 		}
 	}
 }
