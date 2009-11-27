9b430d7829aea6c0e2683f67e00e277acf98a7de
Bryan J Swift <bryan.j.swift@gmail.com>
2009-11-26 22:40:54 -0500

    Add logging to RepositoryStartupServlet
    Logging informs when session gets opened and closed

--- src/main/scala/wknyc/web/servlets/RepositoryStartupServlet.scala
+++ src/main/scala/wknyc/web/servlets/RepositoryStartupServlet.scala
@@ -1,15 +1,19 @@
 package wknyc.web.servlets
 
 import javax.servlet.{GenericServlet,ServletConfig,ServletContext,ServletRequest => Request,ServletResponse => Response}
+import org.apache.commons.logging.LogFactory
 import wknyc.Config
 
 class RepositoryStartupServlet extends GenericServlet {
-	private var session = Config.Repository.login(Config.Admin,Config.CredentialsWorkspace)
+	private lazy val log = LogFactory.getLog(getClass)
+	private val session = Config.Repository.login(Config.Admin,Config.CredentialsWorkspace)
 	override def init(config:ServletConfig) = {
+		log.info("Repository session opened")
 		super.init(config)
 	}
 	override def service(request:Request,response:Response) = { }
 	override def destroy() = {
+		log.info("Closing Repository session")
 		session.logout
 	}
 }
