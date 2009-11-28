739780603c711c7c74988fba1a50447957aa40f9
Bryan J Swift <bryan.j.swift@gmail.com>
2009-11-27 21:00:44 -0500

    Add ability to edit Client instances
    When a non-empty uuid is passed retrieve the client and update it with
    passed fields.

--- src/main/scala/wknyc/web/servlets/ClientServlet.scala
+++ src/main/scala/wknyc/web/servlets/ClientServlet.scala
@@ -24,17 +24,29 @@ class ClientServlet extends HttpServlet with WkServlet {
 		val result = ClientManager.save(client,http.user)
 		val ctx = result match {
 			case Success(payload,message) =>
+				log.info(String.format("Client saved. UUID: %s",payload.uuid))
 				Map("errors" -> result.errors,"client" -> Some(payload),"get" -> false,"uuid" -> payload.uuid)
 			case Failure(errors,message) =>
+				log.info(String.format("Client failed to save. errors: %s",errors.toString))
 				Map("errors" -> errors,"client" -> Some(client),"get" -> false,"uuid" -> client.uuid)
 		}
 		val view = new VelocityView(http.view)
 		view.render(ctx,request,response)
 	}
 	private def getClient(http:HttpHelper) =
-		Client(
-			ContentInfo.Empty,
-			http.parameter("name"),
-			List[CaseStudy]()
-		)
+		if (http.parameter("uuid") != "") {
+			// set new fields on it
+			val c = ClientManager.get(http.parameter("uuid"))
+			Client(
+				c.contentInfo.modifiedBy(http.user.get),
+				http.parameter("name"),
+				c.caseStudies
+			)
+		} else {
+			Client(
+				ContentInfo.Empty,
+				http.parameter("name"),
+				List[CaseStudy]()
+			)
+		}
 }
