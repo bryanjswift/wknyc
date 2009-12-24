f7e57b3ce275d93d7d3ffda9764f1ce024144131
Bryan J Swift <bryan.j.swift@gmail.com>
2009-12-24 01:06:38 -0500

    Pull valid and invalid CaseStudy instances into vals

--- src/test/scala/wknyc/business/validators/CaseStudyValidatorSpecs.scala
+++ src/test/scala/wknyc/business/validators/CaseStudyValidatorSpecs.scala
@@ -6,6 +6,8 @@ import wknyc.model.{BasicCaseStudy,CaseStudy,Client,ContentInfo}
 
 object CaseStudyValidatorSpecs extends Specification {
 	val client = Client(ContentInfo(Config.Admin),"T",Nil)
+	val invalidCaseStudy = CaseStudy(ContentInfo(Config.Admin),client,"")
+	val validCaseStudy = CaseStudy(ContentInfo(Config.Admin),client,"Case Study")
 	"CaseStudyValidator.validate" should {
 		"throw IllegalArgumentException if not validating CaseStudy" >> {
 			CaseStudyValidator.validate(client) must throwA[IllegalArgumentException]
@@ -14,8 +16,7 @@ object CaseStudyValidatorSpecs extends Specification {
 			CaseStudyValidator.validate(null) must throwA[IllegalArgumentException]
 		}
 		"return Success if name valid" >> {
-			val results =
-				CaseStudyValidator.validate(CaseStudy(ContentInfo(Config.Admin),client,"Case Study"))
+			val results = CaseStudyValidator.validate(validCaseStudy)
 			results mustContain(ValidationSuccess(CaseStudy.Name))
 			results mustContain(ValidationSuccess(CaseStudy.Client))
 		}
