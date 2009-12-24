a3a31ceb5c666b4b3055e975a3fd14e7579c3de2
Bryan J Swift <bryan.j.swift@gmail.com>
2009-12-24 02:11:10 -0500

    Add start of specs for CaseStudy saving

--- src/test/scala/wknyc/business/CaseStudyManagerSpecs.scala
+++ src/test/scala/wknyc/business/CaseStudyManagerSpecs.scala
@@ -2,8 +2,30 @@ package wknyc.business
 
 import org.specs.Specification
 import wknyc.Config
+import wknyc.model.{CaseStudy,Client,ContentInfo}
 
 object CaseStudyManagerSpecs extends Specification with Sessioned {
+	val validClient = ClientManagerSpecs.validClient
+	def validCaseStudy(client:Client) = CaseStudy(ContentInfo(Config.Admin),client,"Case Study")
+	val invalidCaseStudy = validators.CaseStudyValidatorSpecs.invalidCaseStudy
 	"CaseStudyManager.save" should {
+		"produce a Failure when not given a user" >> {
+			val response = CaseStudyManager.save(validCaseStudy(validClient),None)
+			response.errors.size must beGreaterThan(0)
+			response.errors mustExist(_.field == "user")
+		}
+		"produce a Failure when given an invalid CaseStudy" >> {
+			val response = CaseStudyManager.save(invalidCaseStudy,Some(Config.Admin))
+			response.errors.size must beGreaterThan(0)
+			response.errors mustExist(_.field == CaseStudy.Name)
+		}
+		"succeed when given a valid CaseStudy and User" >> {
+			sessioned {
+				val client = ClientManager.save(validClient,Some(Config.Admin)).payload
+				val response = CaseStudyManager.save(validCaseStudy(client),Some(Config.Admin))
+				response.errors.size mustBe 0
+				response.payload.uuid must beSome[String]
+			}
+		}
 	}
 }
