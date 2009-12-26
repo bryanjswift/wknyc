fd63c35884dfa91b824b3e69aad5fff51dda7cca
Bryan J Swift <bryan.j.swift@gmail.com>
2009-12-26 17:24:32 -0500

    Remove Normal status, it was meaningless

--- src/main/scala/wknyc/model/CaseStudyStatus.scala
+++ src/main/scala/wknyc/model/CaseStudyStatus.scala
@@ -4,15 +4,13 @@ sealed abstract class CaseStudyStatus(val id:Long)
 
 object CaseStudyStatus {
 	case object New extends CaseStudyStatus(0L)
-	case object Normal extends CaseStudyStatus(1L)
-	case object CopyComplete extends CaseStudyStatus(2L)
-	case object ArtComplete extends CaseStudyStatus(3L)
-	case object Complete extends CaseStudyStatus(4L)
-	case object Published extends CaseStudyStatus(5L)
+	case object CopyComplete extends CaseStudyStatus(1L)
+	case object ArtComplete extends CaseStudyStatus(2L)
+	case object Complete extends CaseStudyStatus(3L)
+	case object Published extends CaseStudyStatus(4L)
 	def apply(id:Long) =
 		id match {
 			case New.id => New
-			case Normal.id => Normal
 			case CopyComplete.id => CopyComplete
 			case ArtComplete.id => ArtComplete
 			case Complete.id => Complete
