138c25ed0947294dc50a03e8aa15374d71fdc757
Bryan J Swift <bryan.j.swift@gmail.com>
2010-02-01 23:07:40 -0500

    Add ability to retrieve information for an existing user by uuid

--- src/main/scala/wknyc/web/servlets/EditUserServlet.scala
+++ src/main/scala/wknyc/web/servlets/EditUserServlet.scala
@@ -9,11 +9,14 @@ import wknyc.model.User._
 import wknyc.model.{ContentInfo,Employee,PersonalInfo,UserRole,WkCredentials}
 
 class EditUserServlet extends HttpServlet with WkServlet {
+	override lazy val html = "user/edit.html.vm"
 	override def doGet(request:Request, response:Response) {
 		val http = HttpHelper(request,response)
 		val employee = UserManager.get(http.data)
-		val context = Map("user" -> employee)
+		val context = Map("uuid" -> http.data)
 		val velocity = new VelocityView(http.view)
 		velocity.render(context,request,response)
+	}
+	override def doPost(request:Request,response:Response) {
 	}
 }
