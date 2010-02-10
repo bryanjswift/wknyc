6cafa41aa7dcb27da2fecd832775eeb1ceefa6c9
Bryan J Swift <bryan.j.swift@gmail.com>
2010-02-09 22:56:18 -0500

    DRY out doGet and doPost
    Move the logic for error handling of retrieving a user out of both doGet
    and doPost and into it's own private method which accepts a function
    which handles the Success case.

--- src/main/scala/wknyc/web/servlets/EditUserServlet.scala
+++ src/main/scala/wknyc/web/servlets/EditUserServlet.scala
@@ -12,33 +12,19 @@ class EditUserServlet extends HttpServlet with UserServlet {
 	override lazy val html = "user/edit.html.vm"
 	override def doGet(request:Request, response:Response) {
 		val http = HttpHelper(request,response)
-		val result = UserManager.get(http.data)
-		val context =
-			result match {
-				case Failure(errors,message) =>
-					log.info(String.format("Unable to retrieve User for UUID: %s",http.data))
-					Map("errors" -> errors,"user" -> None,"uuid" -> Some(http.data))
-				case Success(user,message) =>
-					log.info(String.format("Retrieved User for UUID: %s",http.data))
-					Map("errors" -> result.errors,"user" -> Some(user), "uuid"-> Some(http.data))
-			}
+		val context = get(http.data,http) { retrieve(_,_) }
 		val velocity = new VelocityView(http.view)
 		velocity.render(context,request,response)
 	}
 	override def doPost(request:Request,response:Response) {
 		val http = HttpHelper(request,response)
 		val result = UserManager.get(http.parameter("uuid"))
-		val context =
-			result match {
-				case Failure(errors,message) =>
-					log.info(String.format("Unable to retrieve User for UUID: %s",http.parameter("uuid")))
-					Map("errors" -> errors,"user" -> None,"uuid" -> Some(http.parameter("uuid")))
-				case Success(user,message) =>
-					log.info(String.format("Retrieved User for UUID: %s",http.parameter("uuid")))
-					save(user.asInstanceOf[Employee],http)
-			}
+		val context = get(http.parameter("uuid"),http) { save(_,_) }
 		val velocity = new VelocityView(http.view)
 		velocity.render(context,request,response)
+	}
+	private def retrieve(user:Employee,http:HttpHelper) = {
+		Map("errors" -> Nil,"user" -> Some(user), "uuid"-> Some(http.data))
 	}
 	private def save(user:Employee,http:HttpHelper) = {
 		val employee = getUser(http)
@@ -62,6 +48,17 @@ class EditUserServlet extends HttpServlet with UserServlet {
 			case Failure(errors,message) =>
 				log.info(String.format("User failed to save. errors: %s",errors.toString))
 				Map("errors" -> errors,"user" -> Some(employee),"uuid" -> Some(http.parameter("uuid")))
+		}
+	}
+	private def get(uuid:String,http:HttpHelper)(f:(Employee,HttpHelper) => Map[String,Any]) = {
+		val result = UserManager.get(uuid)
+		result match {
+			case Failure(errors,message) =>
+				log.info(String.format("Unable to retrieve User for UUID: %s",uuid))
+				Map("errors" -> errors,"user" -> None,"uuid" -> Some(uuid))
+			case Success(user,message) =>
+				log.info(String.format("Retrieved User for UUID: %s",uuid))
+				f(user.asInstanceOf[Employee],http)
 		}
 	}
 }
